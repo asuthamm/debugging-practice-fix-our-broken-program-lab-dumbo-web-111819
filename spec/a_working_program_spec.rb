6be79c0ce57e16ad67f662d507f36494c9a2ed93
Alex Suthammanont <asuthamm@gmail.com>
2019-11-05 03:55:44 +0000

    Automatically backed up by Learn

--- spec/a_working_program_spec.rb
+++ spec/a_working_program_spec.rb
@@ -1,6 +1,6 @@
-describe 'A working program' do
+describe 'A 1working program' do
 
-  it 'not to raise an error when loaded' do
+  it 'not 1to raise an error when loaded' do
     expect{
       load 'lib/a_working_program.rb'
     }.to_not raise_error
